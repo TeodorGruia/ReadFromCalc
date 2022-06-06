d593c0d9fa42a0a93179a4bf5610c23273e1b8d2
TeodorGruia <teodorgruia@gmail.com>
2022-06-06 09:29:54 -0400

    The Base template for this script

--- src/App.java
+++ src/App.java
@@ -4,41 +4,9 @@ import java.io.File;
 import java.io.FileReader;
 public class App {
     public static void main(String[] args) throws Exception {
-      
-      System.out.println("Menu\n");
-      displayMenu();
-
-      //Perform an action using a menu
-      String action = "";
-      while (!action.equalsIgnoreCase("exit")) {
-          action = Console.getString("Enter a command: ");
-          System.out.println();
-          
-          if (action.equalsIgnoreCase("r")) {
-            readRecords();
-          } else if (action.equalsIgnoreCase("c")) {
-            System.out.println("This function is still in progress");
-            //doCalculations();
-          } else if (action.equalsIgnoreCase("exit")) {
-              System.out.println("\n");
-          }
-      }
+      BufferedReader in = new BufferedReader(new FileReader("src\\nums.txt"));
+      String line = in.readLine();
 
     }
-    //public String readRecords(BufferedReader Reader) {
-        //BufferedReader in = new BufferedReader(new FileReader("src\\nums.txt"));
-        //String Line = in.readLine();
-        return Line;
-      // in.close();
-    }
-    public static void doCalculations() {
-       //BufferedReader in = new BufferedReader(new FileReader("src\\nums.txt"));
-       //String line = in.readLine();
-       
-    }
-    public static void displayMenu() {
-        System.out.println("r - Read Numbers");
-        System.out.println("c - Calculate Numbers");
-        System.out.println("exit - Exit application");
-}
-}
+  }
+  
